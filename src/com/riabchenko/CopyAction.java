7e81a47f5e316eea389850e41890b117abf8a22a
Denis Riabchenko <driabchenko@exadel.com>
2015-03-09 20:39:57 +0200

    CopyValue like action

--- src/com/riabchenko/CopyAction.java
+++ src/com/riabchenko/CopyAction.java
@@ -1,17 +1,17 @@
 package com.riabchenko;
 
+import com.google.gson.Gson;
 import com.intellij.debugger.DebuggerBundle;
 import com.intellij.debugger.engine.DebugProcess;
-import com.intellij.debugger.engine.evaluation.EvaluateException;
 import com.intellij.debugger.engine.evaluation.EvaluateExceptionUtil;
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl;
 import com.intellij.debugger.impl.DebuggerContextImpl;
-import com.intellij.debugger.impl.DebuggerUtilsEx;
 import com.intellij.debugger.ui.impl.watch.DebuggerTreeNodeImpl;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
 import com.sun.jdi.*;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,24 +19,23 @@ public class CopyAction extends BaseCopyAction {
     private static final Key TO_STRING_METHOD_KEY = new Key("CachedToStringMethod");
 
     protected String processText(Project project, Value value, DebuggerTreeNodeImpl debuggerTreeNode, DebuggerContextImpl debuggerContext) {
-        // OK
-        //return DebuggerUtilsEx.getValueOrErrorAsString(debuggerContext.createEvaluationContext(), value);
-
-        // NOT OK
-        //  LinkageError: loader constraint violation: loader (instance of com/intellij/ide/plugins/cl/PluginClassLoader) previously initiated loading for a different type with name "com/sun/jdi/Value": loader constraint violation: loader (instance of com/intellij/ide/plugins/cl/PluginClassLoader) previously initiated loading for a different type with name "com/sun/jdi/Value"
-        return getValueOrErrorAsString(debuggerContext.createEvaluationContext(), value);
+        Gson gson = new Gson();
+        return gson.toJson(getValueOrError(debuggerContext.createEvaluationContext(), value));
     }
 
 
-    private String getValueOrErrorAsString(EvaluationContextImpl evaluationContext, Value value) {
+    private Object getValueOrError(EvaluationContextImpl evaluationContext, Value value) {
+        Object result = null;
         try {
-            try {
-                if (value == null)
-                    return "null";
-            } catch (ObjectCollectedException ignored) {
-                throw EvaluateExceptionUtil.OBJECT_WAS_COLLECTED;
-            }
             if (value instanceof ObjectReference) {
+                if (value instanceof ArrayReference) {
+                    List<Object> list = new ArrayList<Object>();
+                    for (Value element : ((ArrayReference)value).getValues()) {
+                        list.add(getValueOrError(evaluationContext, element));
+                    }
+                    return list;
+                }
+
                 ObjectReference objRef = (ObjectReference) value;
                 DebugProcess debugProcess = evaluationContext.getDebugProcess();
                 Method toStringMethod = (Method) debugProcess.getUserData(TO_STRING_METHOD_KEY);
@@ -54,21 +53,23 @@ public class CopyAction extends BaseCopyAction {
                     throw EvaluateExceptionUtil.createEvaluateException(DebuggerBundle.message("evaluation.error.cannot.evaluate.tostring", new Object[]{
                             objRef.referenceType().name()
                     }));
-                Value result = debugProcess.invokeInstanceMethod(evaluationContext, objRef, toStringMethod, Collections.emptyList(), 0);
-                if (result == null)
-                    return "null";
-                return (result instanceof StringReference) ? ((StringReference) result).value() : result.toString();
+                result = debugProcess.invokeInstanceMethod(evaluationContext, objRef, toStringMethod, Collections.emptyList(), 0);
             }
-        } catch (EvaluateException evaluateexception) {
-            return evaluateexception.getMessage();
+        } catch (Exception ex) {
+            result = ex.getMessage();
         }
-        return "null";
+
+        if (result == null) {
+            return "null";
+        } else {
+            return (result instanceof StringReference) ? ((StringReference) result).value() : result.toString();
+        }
     }
 
     public static Method findMethod(ReferenceType refType, String methodName, String methodSignature) {
         Method method;
         if (refType instanceof ArrayType) {
-            method = findMethod((ReferenceType) refType.virtualMachine().classesByName("java.lang.Object").get(0), methodName, methodSignature);
+            method = findMethod(refType.virtualMachine().classesByName("java.lang.Object").get(0), methodName, methodSignature);
             if (method != null)
                 return method;
         }
