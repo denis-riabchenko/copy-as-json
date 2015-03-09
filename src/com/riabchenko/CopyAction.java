f0edc37aaf081746e2bbb614e60bf094491ba987
Denis Riabchenko <driabchenko@exadel.com>
2015-03-09 17:03:12 +0200

    Initial project structure

---
+++ src/com/riabchenko/CopyAction.java
@@ -0,0 +1,10 @@
+package com.riabchenko;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class CopyAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
