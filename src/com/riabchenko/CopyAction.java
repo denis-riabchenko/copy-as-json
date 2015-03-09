255aa18bf6255646ebf8a1bead94c1079f28eab5
Denis Riabchenko <driabchenko@exadel.com>
2015-03-09 18:03:15 +0200

    CopyAction from CE (not working due to NPE)

--- src/com/riabchenko/CopyAction.java
+++ src/com/riabchenko/CopyAction.java
@@ -1,10 +1,53 @@
 package com.riabchenko;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.ide.CopyPasteManager;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.awt.datatransfer.StringSelection;
 
 public class CopyAction extends AnAction {
+    public static final DataKey<DefaultMutableTreeNode> SELECTED_NODE = DataKey.create("SELECTED_NODE");
+
+    public CopyAction() {
+    }
+
+    public CopyAction(JComponent component) {
+        final AnAction action = ActionManager.getInstance().getAction("$Copy");
+        if (action != null) {
+            copyFrom(action);
+            registerCustomShortcutSet(getShortcutSet(), component);
+        }
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        e.getPresentation().setEnabled(isEnabled(e));
+    }
+
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        final DefaultMutableTreeNode node = e.getData(SELECTED_NODE);
+        Object obj = node.getUserObject();
+//        if (node instanceof GeneratedStructureModel.StructureNode) {
+//            final GeneratedStructureModel.StructureNode structureNode = (GeneratedStructureModel.StructureNode)node;
+//            final OutputEventQueue.NodeEvent event = structureNode.getUserObject();
+//            setClipboardData(event.getValue());
+//        }
+    }
+
+    private static void setClipboardData(String value) {
+        CopyPasteManager.getInstance().setContents(new StringSelection(value));
+    }
+
+    protected static boolean isEnabled(AnActionEvent e) {
+        return true;
+//        final DefaultMutableTreeNode node = e.getData(SELECTED_NODE);
+//        if (node instanceof GeneratedStructureModel.StructureNode) {
+//            final GeneratedStructureModel.StructureNode structureNode = (GeneratedStructureModel.StructureNode)node;
+//            final OutputEventQueue.NodeEvent event = structureNode.getUserObject();
+//            return event != null && event.getValue() != null;
+//        }
+//        return false;
     }
 }
